fe2d7e7a5d5c569176c9a1257fb7ed2d78cf7156
Ryuzen2k22 <111623948+Ryuzen2k22@users.noreply.github.com>
2022-09-20 20:15:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,19 +2,20 @@ global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
-  '62857139649630': { // put your number here
-    name: 'Owner Metro Bot/Creator games-wabot',
+  '6285727231608': { // put your number here
+    name: 'Owner Ryuzen2k22/Creator konosuba-bot',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: false, // if true this number will send if user use command /report or /join
     isCreator: true // if true this number will send if user use command /owner or /creator
   },
-  '6281390658325': { // put your number here
+  '6285327068982': { // put your number here
+    name: 'NanNi',
     isDev: true, // if true this number will send if bot have bug and developer mode true
     isReport: true, // if true this number will send if user use command /report or /join
     isCreator: false // if true this number will send if user use command /owner or /creator
   },
-  '6282256115584': { // put your number here
-    name: 'Dave',
+  '62859106594173': { // put your number here
+    name: 'konosuba-bot',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: true, // if true this number will send if user use command /report or /join
     isCreator: false // if true this number will send if user use command /owner or /creator
@@ -56,7 +57,7 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot Is The Best'
+global.author = 'Kono Subarashii Sekai ni Shukufuku wo'
 
 global.multiplier = 39 // The higher, The harder levelup
 
